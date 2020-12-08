2129e3f508c7e6c74d808abd519e24be6d4fc909
vivan-1 <superzanark22@gmail.com>
2020-12-09 03:46:26 +0530

    make it into heroku:

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -9,7 +10,8 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'mysecretkey'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Vivekshukla@123@localhost/calc'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Vivekshukla@123@localhost/calc'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
